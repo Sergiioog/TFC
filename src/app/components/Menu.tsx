884055d8b5fa0060073f523c392601d74dfbd900
Sergiioog <sergiogargor9@gmail.com>
2024-01-24 20:17:19 +0100

    Prueba Header

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,19 @@
+
+export default function Menu() {
+    return(
+        <div>
+            <figure>
+                <img src="" alt=""></img>
+            </figure>
+            <ul className="bg-white text-black min-h-20 max-h-20 w-full flex justify-center items-center">
+                <li className="font-bold text-2xl ml-3 mr-3">Inicio</li>
+                <li className="font-bold text-2xl ml-3 mr-3">Diario</li>
+                <li className="font-bold text-2xl ml-3 mr-3">Ejercicios</li>
+                <li className="font-bold text-2xl ml-3 mr-3">Estiramientos</li>
+                <li className="font-bold text-2xl ml-3 mr-3">Usuario</li>
+            </ul>
+        </div>
+       
+    );
+    
+}
