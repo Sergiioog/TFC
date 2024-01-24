951ee493b8458a04163f9802e308030202b13e19
Sergiioog <sergiogargor9@gmail.com>
2024-01-24 21:33:59 +0100

    Header

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Menu() {
     return (
@@ -7,10 +8,26 @@ export default function Menu() {
                 <Image src={"assets/iconos/next.svg"} alt="imagen" width={120} height={120} />
             </figure>
             <ul className="bg-white text-black h-full flex justify-center items-center space-x-8">
-                <li className="font-bold text-xl "><a href="">Inicio</a></li>
-                <li className="font-bold text-xl "><a href="">Diario</a></li>
-                <li className="font-bold text-xl "><a href="">Ejercicios</a></li>
-                <li className="font-bold text-xl "><a href="">Estiramientos</a></li>
+                <li className="font-bold text-xl">
+                    <Link href="/">
+                        <p>Inicio</p>
+                    </Link>
+                </li>
+                <li className="font-bold text-xl">
+                    <Link href="/">
+                        <p>Diario</p>
+                    </Link>
+                </li>
+                <li className="font-bold text-xl">
+                    <Link href="/ejercicios">
+                        <p>Ejercicios</p>
+                    </Link>
+                </li>
+                <li className="font-bold text-xl">
+                    <Link href="/">
+                        <p>Estiramientos</p>
+                    </Link>
+                </li>
             </ul>
             <div className="flex items-center mr-4">
                 <button className="bg-slate-200 rounded-lg text-black text-md p-1 mr-2"><a href="">REGISTRARSE</a></button>
