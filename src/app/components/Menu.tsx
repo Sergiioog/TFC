af21e15833ecac918ea643cab7f339c59f6d2725
Sergiioog <sergiogargor9@gmail.com>
2024-01-24 21:24:24 +0100

    Seguimos con el Header

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,23 +1,21 @@
+import Image from "next/image";
 
 export default function Menu() {
-    return(
-        <div className="bg-red-500 min-h-12 max-h-12 flex justify-around items-center">
-            <figure>
-                <img src="" alt=""></img>
+    return (
+        <div className="bg-white min-h-12 max-h-12 flex justify-between items-center px-4">
+            <figure className="ml-4">
+                <Image src={"assets/iconos/next.svg"} alt="imagen" width={120} height={120} />
             </figure>
-            <ul className="bg-white text-black h-full w-fit flex justify-center items-center">
-                <li className="font-bold text-xl ml-3 mr-3">Inicio</li>
-                <li className="font-bold text-xl ml-3 mr-3">Diario</li>
-                <li className="font-bold text-xl ml-3 mr-3">Ejercicios</li>
-                <li className="font-bold text-xl ml-3 mr-3">Estiramientos</li>
-                <li className="font-bold text-xl ml-3 mr-3">Usuario</li>
+            <ul className="bg-white text-black h-full flex justify-center items-center space-x-8">
+                <li className="font-bold text-xl "><a href="">Inicio</a></li>
+                <li className="font-bold text-xl "><a href="">Diario</a></li>
+                <li className="font-bold text-xl "><a href="">Ejercicios</a></li>
+                <li className="font-bold text-xl "><a href="">Estiramientos</a></li>
             </ul>
-            <div className="bg-blue-200 h-full">
-                <button className="bg-green-200 mr-2 ml-2 rounded-lg w-fit text-black text-md p-1">REGISTRARSE</button>
-                <button className="bg-green-200 mr-2 ml-2 rounded-lg w-fit text-black text-md p-1">INICIAR SESIÓN</button>
+            <div className="flex items-center mr-4">
+                <button className="bg-slate-200 rounded-lg text-black text-md p-1 mr-2"><a href="">REGISTRARSE</a></button>
+                <button className="bg-slate-200 rounded-lg text-black text-md p-1"><a href="">INICIAR SESIÓN</a></button>
             </div>
         </div>
-       
     );
-    
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <main className="min-h-screen h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      {/* bg-white min-h-screen h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] */}
         <Menu />
       </main>
     </div>
